c669a62ae31e2d612dc436ee0ae7d32f0b3f0d19
Prem Chavhan <pchavhan@logiquad.com>
2023-12-28 13:05:10 +0530

    Updated fixes

--- tests/build.sh
+++ tests/build.sh
@@ -3,4 +3,5 @@
 #!/usr/bin/env bash
 rm -f Dockerfile
 cp ./docker/Dockerfile ./
+sed -i 's/CMD "flowise"/CMD "sleep 3; flowise start"/' Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/flowiseai:latest | docker load
