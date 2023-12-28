9f0a846ddf3781b1e3d2a0cc23c067034633488c
Prem Chavhan <pchavhan@logiquad.com>
2023-12-28 11:43:03 +0530

    Updated fixes

--- tests/build.sh
+++ tests/build.sh
@@ -1,2 +1,6 @@
-sed -i 's/yarn install/yarn install --network-timeout 100000/g' ./Dockerfile
+#sed -i 's/yarn install/yarn install --network-timeout 100000/g' ./Dockerfile
+#docker buildx build . --output type=docker,name=elestio4test/flowiseai:latest | docker load
+#!/usr/bin/env bash
+rm -f Dockerfile
+cp ./docker/Dockerfile ./
 docker buildx build . --output type=docker,name=elestio4test/flowiseai:latest | docker load
